9e28b6ef6b6ecda8aaf83d7895b98b4b64b04201
zhenhangguo <zhenhang.guo@plus.ai>
2025-03-25 16:52:05 +0800

    add LSTM to compress history input before embedding

--- car_foundation/car_foundation/models.py
+++ car_foundation/car_foundation/models.py
@@ -121,7 +121,7 @@ class LearnedPositionalEncoding(nn.Module):
 
 class TorchTransformerDecoder(nn.Module):
     def __init__(self, state_dim, action_dim, output_dim, latent_dim, num_heads, 
-                num_layers, device, dropout=0.1, history_length=250, prediction_length=50):
+                num_layers, device, dropout=0.1, history_length=250, prediction_length=50, compressed_history_length = 50):
         super().__init__()
         # 初始化维度参数
         self.device = device
@@ -131,19 +131,39 @@ class TorchTransformerDecoder(nn.Module):
         self.latent_dim = latent_dim
         self.history_length = history_length
         self.prediction_length = prediction_length
+        self.compressed_history_length = compressed_history_length
+        self.stride = self.history_length // self.compressed_history_length
 
         self.embedding = nn.ModuleDict({
             'state': nn.Linear(state_dim, latent_dim),
             'action': nn.Linear(action_dim, latent_dim),
             'output': nn.Linear(latent_dim, output_dim)
         }).to(device)
+
+        # 新增历史状态压缩器
+        self.history_state_compressor = nn.LSTM(
+            input_size=state_dim,
+            hidden_size=latent_dim,
+            num_layers=1,
+            batch_first=True,
+            bidirectional=False
+        ).to(device)
+
+        # 新增历史动作压缩器
+        self.history_action_compressor = nn.LSTM(
+            input_size=action_dim,
+            hidden_size=latent_dim,
+            num_layers=1,
+            batch_first=True,
+            bidirectional=False
+        ).to(device)
         
         self.register_buffer('tgt_mask', 
                            nn.Transformer.generate_square_subsequent_mask(prediction_length).to(device))
         
         # 位置编码
         self.position_encoding = nn.ModuleDict({
-            'history': LearnedPositionalEncoding(latent_dim, history_length * 2 - 1, flip=True).to(device) ,
+            'history': LearnedPositionalEncoding(latent_dim, compressed_history_length * 2 - 1, flip=True).to(device) ,
             'action': LearnedPositionalEncoding(latent_dim, prediction_length).to(device) 
         })
         
@@ -164,10 +184,16 @@ class TorchTransformerDecoder(nn.Module):
 
     def _build_history_emb(self, history: torch.Tensor) -> torch.Tensor:
         """向量化的历史序列构建"""
-        state_emb = self.embedding['state'](history[..., :self.state_dim])
-        action_emb = self.embedding['action'](history[..., self.state_dim:])
-        
-        interleaved = torch.stack([state_emb, action_emb], dim=2)
+        # state_emb = self.embedding['state'](history[..., :self.state_dim])
+        # action_emb = self.embedding['action'](history[..., self.state_dim:])
+        # 经过LSTM网络直接输出嵌入后的结果
+        compress_state_emb_seq, _ = self.history_state_compressor(history[..., :self.state_dim])
+        compress_action_emb_seq, _ = self.history_action_compressor(history[..., self.state_dim:]) 
+
+        compress_state_emb = compress_state_emb_seq[:,::self.stride, :]
+        compress_action_emb = compress_action_emb_seq[:,::self.stride, :]
+
+        interleaved = torch.stack([compress_state_emb, compress_action_emb], dim=2)
         interleaved = interleaved.view(interleaved.size(0), -1, interleaved.size(-1))
         return interleaved[:, :-1, :]
 
